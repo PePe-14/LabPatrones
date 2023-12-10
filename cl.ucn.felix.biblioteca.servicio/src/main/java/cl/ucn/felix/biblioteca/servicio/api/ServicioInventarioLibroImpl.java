ee65e64248cf02f7dcfcc10625f01309ca3fd18e
pipeta <felipe.tapia.guerra@gmail.com>
2023-12-10 11:30:15 -0800

    first commit

--- cl.ucn.felix.biblioteca.servicio/src/main/java/cl/ucn/felix/biblioteca/servicio/api/ServicioInventarioLibroImpl.java
+++ cl.ucn.felix.biblioteca.servicio/src/main/java/cl/ucn/felix/biblioteca/servicio/api/ServicioInventarioLibroImpl.java
@@ -9,6 +9,18 @@ import cl.ucn.felix.biblioteca.api.ExcepcionLibroNoEncontrado;
 import cl.ucn.felix.biblioteca.api.Inventario;
 import cl.ucn.felix.biblioteca.api.Libro;
 
+import cl.ucn.felix.biblioteca.api.LibroMutable;
+import cl.ucn.felix.biblioteca.api.ExcepcionLibroInvalido;
+import cl.ucn.felix.biblioteca.api.ExcepcionLibroNoEncontrado;
+import cl.ucn.felix.biblioteca.api.Inventario.CriterioBusqueda; 
+import cl.ucn.felix.biblioteca.api.ExcepcionLibroExistente;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+
+
 public class ServicioInventarioLibroImpl implements ServicioInventarioLibro{
 
 	private String sesion;
@@ -55,56 +67,116 @@ public class ServicioInventarioLibroImpl implements ServicioInventarioLibro{
 	@Override
 	public Set<String> obtenerGrupos(String sesion)  {
 		// TODO Auto-generated method stub
-		return null;
+		
+			Inventario inventario;
+			try {
+				inventario = buscarLibroEnInventario();
+				return inventario.getCategorias();
+			} catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+				
+				e.printStackTrace();
+				return null;
+			}
+			
+
 		
 	}
+	
+	@Override
+	public void removerLibro(String sesion, String isbn) {
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        inventario.removerLibro(isbn);
+	    } catch (ExcepcionLibroNoEncontrado e) {
+	        e.printStackTrace();
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	@Override
+	public Libro obtenerLibro(String sesion, String isbn) throws ExcepcionLibroNoEncontrado, ExcepcionSesionNoValidaTiempoEjecucion {
+	    try {
+	        this.chequearSesion(sesion);
+	        Inventario inventario = buscarLibroEnInventario();
+	        return inventario.cargarLibro(isbn);
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion | ExcepcionLibroNoEncontrado e) {
+	        e.printStackTrace();
+	        throw e; 
+	    }
+	}
+	@Override
+	public void modificarCategoriaLibro(String sesion, String isbn, String categoria) {
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        LibroMutable libro = inventario.cargarLibroParaEdicion(isbn);
+	        libro.setCategoria(categoria);
+	        inventario.guardarLibro(libro);
+	    } catch (ExcepcionLibroNoEncontrado | ExcepcionLibroInvalido e) {
+	        e.printStackTrace();
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	    }
+	}
+
 
 	@Override
 	public void adicionarLibro(String sesion, String isbn, String titulo, String autor, String categoria) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void modificarCategoriaLibro(String sesion, String isbn, String categoria) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removerLibro(String sesion, String isbn) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Libro obtenerLibro(String sesion, String isbn) throws ExcepcionLibroNoEncontrado , ExcepcionSesionNoValidaTiempoEjecucion{
-		// TODO Auto-generated method stub
-		this.chequearSesion(sesion);
-		Inventario inventario = buscarLibroEnInventario();
-		return inventario.cargarLibro(isbn);
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        LibroMutable libro = inventario.crearLibro(isbn);
+	        libro.setTitulo(titulo);
+	        libro.setAutor(autor);
+	        libro.setCategoria(categoria);
+	        inventario.guardarLibro(libro);
+	    } catch (ExcepcionLibroInvalido | ExcepcionLibroExistente e) {
+	        e.printStackTrace();
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	    }
 	}
 
 	@Override
 	public Set<String> buscarLibrosPorCategoria(String sesion, String categoriaLike) {
-		// TODO Auto-generated method stub
-		return null;
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        Map<CriterioBusqueda, String> criterio = new HashMap<>();
+	        criterio.put(CriterioBusqueda.CATEGORIA_LIKE, categoriaLike);
+	        return inventario.buscarLibros(criterio);
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	        return null;
+	    }
 	}
 
 	@Override
 	public Set<String> buscarLibrosPorAutor(String session, String autorLike) {
-		// TODO Auto-generated method stub
-		return null;
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        Map<CriterioBusqueda, String> criterio = new HashMap<>();
+	        criterio.put(CriterioBusqueda.AUTOR_LIKE, autorLike);
+	        return inventario.buscarLibros(criterio);
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	        return null; 
+	    }
 	}
 
 	@Override
 	public Set<String> buscarLibrosPorTitulo(String sesion, String tituloLike) {
-		// TODO Auto-generated method stub
-		return null;
+	    try {
+	        Inventario inventario = buscarLibroEnInventario();
+	        Map<CriterioBusqueda, String> criterio = new HashMap<>();
+	        criterio.put(CriterioBusqueda.TITULO_LIKE, tituloLike);
+	        return inventario.buscarLibros(criterio);
+	    } catch (ExcepcionSesionNoValidaTiempoEjecucion e) {
+	        e.printStackTrace();
+	        return null;
+	    }
 	}
 
+
 	private Inventario buscarLibroEnInventario() throws ExcepcionSesionNoValidaTiempoEjecucion {
-		
 		String nombre = Inventario.class.getName();
 		ServiceReference<?> ref = this.contexto.getServiceReference(nombre);
 		if (ref == null) {
