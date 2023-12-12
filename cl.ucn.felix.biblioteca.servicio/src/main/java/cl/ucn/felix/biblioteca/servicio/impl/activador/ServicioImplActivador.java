dda33e9d39ac715238d70a54b26e363d4b458740
pipeta <felipe.tapia.guerra@gmail.com>
2023-12-11 18:08:01 -0800

    video

--- cl.ucn.felix.biblioteca.servicio/src/main/java/cl/ucn/felix/biblioteca/servicio/impl/activador/ServicioImplActivador.java
+++ cl.ucn.felix.biblioteca.servicio/src/main/java/cl/ucn/felix/biblioteca/servicio/impl/activador/ServicioImplActivador.java
@@ -4,22 +4,33 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
+
 import cl.ucn.felix.biblioteca.servicio.api.ServicioInventarioLibro;
+import cl.ucn.felix.biblioteca.servicio.api.ServicioInventarioLibroImpl;
 
-public class ServicioImplActivador  implements BundleActivator {
+public class ServicioImplActivador implements BundleActivator {
 
-	ServiceRegistration<ServicioInventarioLibro> reg = null;
-	
-	@Override
-	public void start(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    private ServiceRegistration<ServicioInventarioLibro> reg = null;
+  
+    @Override
+    public void start(BundleContext context) throws Exception {
+        System.out.println("\nComenzando implementación del Servicio de Inventario de Libros");
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+        ServicioInventarioLibro servicioInventarioLibro = new ServicioInventarioLibroImpl(context);
+        this.reg = context.registerService(
+                ServicioInventarioLibro.class,
+                servicioInventarioLibro,
+                null
+        );
+    }
 
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("\nParando implementación del Servicio de Inventario de Libros");
+        if (this.reg != null) {
+            context.ungetService(reg.getReference());
+            this.reg = null;
+        }
+    }
 }
